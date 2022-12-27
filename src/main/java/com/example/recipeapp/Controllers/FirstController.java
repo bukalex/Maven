0f59ec452b914b9eb9b742ff215edeb885cd9a2e
gurylevalexey <gurylevalexey@gmail.com>
2022-12-27 16:43:25 +0300

    Задание выполнено.

--- src/main/java/com/example/recipeapp/Controllers/FirstController.java
+++ src/main/java/com/example/recipeapp/Controllers/FirstController.java
@@ -5,5 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping
+    public String itWorks(){
+        return "It works!";
+    }
 
+    @GetMapping("/info")
+    public String infoPage(){
+        return "Имя: " + "Алексей" + "<br>" +
+                "Название проекта: " + "Рецепты" + "<br>" +
+                "Дата создания проекта: " + "27.12.2022" + "<br>" +
+                "Описание проекта: " + "Использован язык Java";
+    }
 }
